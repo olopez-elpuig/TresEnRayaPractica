b87cba20cc1d4e784b86a0d3fc4fb95abe5eb7ff
Oscar <olopez@elpuig.xeill.net>
2020-06-04 09:53:39 +0200

    edited

--- src/com/company/Jugador.java
+++ src/com/company/Jugador.java
@@ -25,7 +25,11 @@ public class Jugador {
     }
 
     public void ponerFicha(Tablero tablero){
+//      El metodo nos ayudará a Introducir la ficha y con esto a repetirlo en caso de fallo/error.
         try {
+//          InputStreamReader pasará a leer los bytes a caracteres guardandolos en el system.in de esta manera usaremos tambien el BufferedReader,
+//          hará lo mismo que el InputStreamReader este convertira la linea que le hemos pedido en String y nos la devolverá.
+//          De esta manera usaremos el br.readLine, para pedirle un String, este metodo leerá todos los caracteres tecleados hasta que se presione la tecla INTRO.
             InputStreamReader isr = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader (isr);
             System.out.print("Fila: ");
@@ -33,6 +37,7 @@ public class Jugador {
             System.out.print("Columna: ");
             int columna = Integer.parseInt(br.readLine());
             tablero.getTablero()[fila][columna] = " _"+this.ficha.getFicha()+"_ ";
+//      Este catch nos ayuda a tratar el fallo ocurrido dentro del try
         } catch (IOException ex) {
             Logger.getLogger(Jugador.class.getName()).log(Level.SEVERE, null, ex);
         }
